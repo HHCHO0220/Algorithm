600bd276dc4f17170fdea9b9082f7ea3a623bd4f
Hyunho Cho <109830398+HHCHO0220@users.noreply.github.com>
2022-12-02 00:15:34 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ message:String) -> Int {
+    var result: Int
+    
+    // result = message 스트링의 길이 * 2
+    result = message.count * 2
+    
+    return result
+}
