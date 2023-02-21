9ecba36fd950c25334a1d027eb2611ed243b71ff
Hyunho Cho <109830398+HHCHO0220@users.noreply.github.com>
2023-02-21 13:54:08 +0900

    [level 1] Title: 내적, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/70128. 내적/내적.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ a:[Int], _ b:[Int]) -> Int {
+    var result: Int = 0
+
+    for i in a.indices {
+        result += a[i] * b[i]
+    }
+    
+    return result
+}
