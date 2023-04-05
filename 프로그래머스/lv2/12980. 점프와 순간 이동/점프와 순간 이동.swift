50616fc78a4ea82665bc0b91772e173466200518
Hyunho Cho <109830398+HHCHO0220@users.noreply.github.com>
2023-04-06 00:39:42 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var n: Int = n
+    var result: Int = 0
+
+    while n > 0 {
+        if n % 2 == 0 {
+            n /= 2
+        } else {
+            n -= 1
+            result += 1
+        }
+    }
+
+    return result
+}
