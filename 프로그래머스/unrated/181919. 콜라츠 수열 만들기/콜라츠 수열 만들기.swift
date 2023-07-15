901e27cd4bfa55bb75932758f634f7c3ec34fe7f
Hyunho Cho <109830398+HHCHO0220@users.noreply.github.com>
2023-07-16 02:40:42 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ n:Int) -> [Int] {
+    var n: Int = n
+    var result: [Int] = []
+
+    while n != 1 {
+        result.append(n)
+        if n % 2 == 0 {
+            n /= 2
+        } else {
+            n = 3 * n + 1
+        }
+    }
+    result.append(1)
+    
+    return result
+}
